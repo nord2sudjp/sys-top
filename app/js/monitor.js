867b61353505e050c3745644b741d067e62dd267
maka y <nord2sudjp@gmail.com>
2020-11-17 10:31:41 +0900

    show cpu model

---
+++ app/js/monitor.js
@@ -0,0 +1,8 @@
+const path = require("path");
+const osu = require("node-os-utils");
+const cpu = osu.cpu;
+const mem = osu.mem;
+const os = osu.os;
+
+// Set model
+document.getElementById("cpu-model").innerText = cpu.model();
