b1261d15b5cc4f0b4e3296759bc557a164bb0923
maka y <nord2sudjp@gmail.com>
2020-11-17 11:18:13 +0900

    Implemnet notify frequency

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,7 +4,8 @@ const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
-let cpuOverload = 5;
+let cpuOverload = 3;
+let alertFreq = 1;
 
 // Run every 2 seconds
 
@@ -12,7 +13,24 @@ setInterval(() => {
   cpu.usage().then((info) => {
     document.getElementById("cpu-usage").innerText = info + "%";
     document.getElementById("cpu-progress").style.width = info + "%";
+
+    if (info >= cpuOverload) {
+      document.getElementById("cpu-progress").style.background = "red";
+    } else {
+      document.getElementById("cpu-progress").style.background = "green";
+    }
+
+    if (info >= cpuOverload && runNotify(alertFreq)) {
+      notifyUser({
+        title: "CPU overload",
+        body: `CPU is over ${cpuOverload}%`,
+        icon: path.join(__dirname, "img", "icon.png"),
+      });
+
+      localStorage.setItem("lastNotify", +new Date());
+    }
   });
+
   cpu.free().then((info) => {
     document.getElementById("cpu-free").innerText = info + "%";
   });
@@ -36,4 +54,26 @@ function secToDh(sec) {
   const m = Math.floor((sec % 3600) / 60);
   const s = Math.floor(sec % 60);
   return `${d}d, ${h}h,${m}m,${s}s`;
+}
+
+function notifyUser(options) {
+  console.log("notified");
+  new Notification(options.title, options);
+}
+
+function runNotify(frequency) {
+  if (localStorage.getItem("lastNotify") === null) {
+    localStorage.setItem("lastNotify", +new Date());
+    return true;
+  }
+  const notifytime = new Date(parseInt(localStorage.getItem("lastNotify")));
+  const now = new Date();
+  const difftime = Math.abs(now - notifytime);
+  const minutesPassed = Math.ceil(difftime / (1000 * 60));
+
+  if (minutesPassed > frequency) {
+    return true;
+  } else {
+    return false;
+  }
 }
